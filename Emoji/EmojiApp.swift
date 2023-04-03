c56b3e0d614378856239b0051f0312ebb48ba3e5
Bhargava Charan <bhargavacharan1316@gmail.com>
2023-04-02 22:04:15 -0500

    Add files via upload

---
+++ Emoji/EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiApp.swift
+//  Emoji
+//
+//  Created by charan on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
